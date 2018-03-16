7af64a03a96e9b6b6e6a7d0124c15307cb50a4cd
EthanL1 <32692617+EthanL1@users.noreply.github.com>
2018-03-15 17:25:09 -0700

    render_template push

--- Flask.py
+++ Flask.py
@@ -17,8 +17,16 @@ c = con.cursor()
 
 app = Flask(__name__)
 # Return all results of query
-def dataFrameJson():
-    #url end point
+
+@app.route("/")
+def index():
+
+    return render_template('index.html')   
+
+## this one is in the works as of now but not currently working
+@app.route("/geoJsonBoundary")
+def geoJsonBound():
+     #url end point
     url = 'https://bubinga.co/wp-content/uploads/jsonstates.min_.js'
     #create url enpoint to read in json
     json_file = req.get(url).json()
@@ -26,7 +34,7 @@ def dataFrameJson():
     yelp_list_sql=[]
 
     #sql loop for statement 
-    for data in c.execute("SELECT State , count(name) as location_count, avg (sentiment) as average_sentiment FROM yelp WHERE state in ('AL', 'AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY') group by State  LIMIT 1000"):
+    for data in c.execute("SELECT State , count(name) as location_count, avg (sentiment) as average_sentiment FROM yelp WHERE state in ('AL', 'AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY') group by State  LIMIT 1000000"):
             yelp_Dict_sql_loop = {
                 'STATE':data[0],
                 'location_Count':data[1],
@@ -36,21 +44,22 @@ def dataFrameJson():
         
             yelp_list_sql.append(yelp_Dict_sql_loop)
 
-
-
     #make a data frame off of the json url
     json_df = pd.read_json(url)
 
-
+    print('!!!!!!!!!!!!!!!!!!')
     #take json file and make state append
     json_states = []
+    json_geometry = []
+    print('!!!!!')
     for i in json_df['features']:
         abrrev_Variable= (i['properties']['abbr'])
         json_states.append(abrrev_Variable)
+ 
 
     #add to 
-    json_df['State'] = json_states
-
+    
+    
     #take sql dictonary find the states
     empty_state_dict = []
     for i in yelp_list_sql:
@@ -59,31 +68,37 @@ def dataFrameJson():
 
     yelp_sql_dict = pd.DataFrame.from_dict(yelp_list_sql)
     yelp_sql_dict['State']= empty_state_dict
+    
+    print('*********')
+    json_df.reset_index(drop=True)
+    print('#$#$#$$##')
+    json_df['State'] = json_states
+    print('*********')
+   
+    print('#$#$#$$##')
+    json_df['Geometry'] = json_geometry
 
+    print('sssssssssssss')
+   
+  
     #merge the two data frames on added state column
     combined_state = pd.merge(yelp_sql_dict,json_df,how='inner', on='State' )
     #drop state column
     combined_state=combined_state.drop(['STATE'],axis=1)
-    return combined_state
-
-
-
-#this route isn't functional 
-@app.route('/test')
-def showjson():
+    combined_state = combined_state.drop(['features'], axis=1)
+    print('!!!!!!!!!!!!!!!!!!')
+    polygon_list =[]
+    #itterate through each row and make a dictonary out of the each column/row
+    for index, row in combined_state.iterrows():
+        poly_Dict = {
+            'STATE':row['STATE'],
+            'Location_Count':row['location_Count'],
+            'Average_Sentiment':row['Average_Sentiment'],
+            'Geometry':row['Geometry']
+            }
+        polygon_list.append(poly_Dict)
     
-    json_url = 'geoJsonCoord.json'
-    data_json = json.load(open(json_url))
-    print('*************')
-    return jsonify(data_json)
-    
-## this one is in the works as of now but not currently working
-@app.route("/geoJsonBoundary")
-def geoJsonBound():
-    test = dataFrameJson()
-    print("**************")
-    print(test['features'])
-    return jsonify({test['features']['properties']})
+    return jsonify(polygon_list)
 
     
 #endpoint is to create markets for each location in the list..
@@ -95,7 +110,7 @@ def geoJson():
    
     yelp_list=[]
     
-    for data in c.execute("SELECT NAME, City, State, Latitude, Longitude, Stars, Review_Count, Bucket, Sentiment FROM yelp WHERE  state in ('AL', 'AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY')  LIMIT 500 OFFSET 1"):
+    for data in c.execute("SELECT NAME, City, State, Latitude, Longitude, Stars, Review_Count, Bucket, Sentiment FROM yelp WHERE  state in ('AL', 'AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY')  LIMIT 5000 OFFSET 1"):
         yelp_Dict = {
             'properties': {'Name':data[0],
             'address': {'City':data[1],
@@ -122,7 +137,7 @@ def cityjson():
    
     yelp_list=[]
     
-    for data in c.execute("SELECT State , count(name) as location_count, avg (sentiment) as average_sentiment FROM yelp WHERE state in ('AL', 'AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY') group by State  LIMIT 2000"):
+    for data in c.execute("SELECT State , count(name) as location_count, avg (sentiment) as average_sentiment FROM yelp WHERE state in ('AL', 'AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY') group by State  LIMIT 1000000"):
         yelp_Dict = {
             'STATE':data[0],
             'location_Count':data[1],
